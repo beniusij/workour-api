8edc64e94f195ed0f06e071443048637580088d3
Juozas Joe Beniusis <beniusij@gmail.com>
2019-10-26 20:19:45 +0100

    Add token validation and related test

--- common/token.go
+++ common/token.go
@@ -1,5 +1,46 @@
 package common
 
-func GenerateToken(email string) string {
-	return ""
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"os"
+	"time"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+func GenerateToken(email string) (string, error) {
+	claims := jwt.MapClaims{
+		"email": email,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) bool {
+	if tokenString == "" {
+		return false
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return false
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	return claims.VerifyExpiresAt(jwt.TimeFunc().Unix(), false)
 }
--- tests/token_test.go
+++ tests/token_test.go
@@ -15,13 +15,32 @@ var tokenRegex = `^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$`
 
 func TestGenerateToken(t *testing.T) {
 	asserts := getAsserts(t)
+	token, err := comm.GenerateToken(creds["email"].(string))
 
 	// Verify that generated string matches JWT token regex
-	token := comm.GenerateToken(creds["email"].(string))
-	asserts.NotNil(token, "token is not nill")
+	asserts.NoError(err, "no error is returned")
+	asserts.NotNil(token, "token is not nil")
 	asserts.Regexp(tokenRegex, token, "JWT token matches token regex")
 
 	// Verify that the JWT contains three segments, separated by two period ('.') characters
 	tokenSlice := strings.Split(token, ".")
 	asserts.EqualValues(3, len(tokenSlice), "JWT token contains 3 parts separated by '.'")
+}
+
+func TestValidateToken(t *testing.T) {
+	asserts := getAsserts(t)
+	token, _ := comm.GenerateToken(creds["email"].(string))
+
+	validToken := comm.ValidateToken(token)
+	invalidToken := comm.ValidateToken("Potato123")
+
+	asserts.True(validToken, "token is valid")
+	asserts.False(invalidToken, "token is invalid")
+}
+
+func TestDecodeToken(t *testing.T) {
+	asserts := getAsserts(t)
+	token, _ := comm.GenerateToken(creds["email"].(string))
+
+	// Verify
 }
