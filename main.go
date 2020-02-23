6ad5d75aa14146ccb2d74dcad32945420ece46db
Juozas Joe Beniusis <jb@bluetel.co.uk>
2020-02-23 18:41:26 +0000

    Move routes into separate package

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/graphql-go/graphql"
 	"github.com/jinzhu/gorm"
 	"github.com/subosito/gotenv"
 	"os"
-	auth "workour-api/authentication"
 	comm "workour-api/common"
-	g "workour-api/gql"
+	"workour-api/config"
 	u "workour-api/users"
+)
+
+var (
+	appPort = os.Getenv("GIN_PORT")
+
 )
 
 func init() {
@@ -41,39 +43,9 @@ func initAPI() (*gin.Engine, *gorm.DB) {
 	router := gin.Default()
 
 	// Set up Redis client for sessions
-	redisHost := os.Getenv("REDIS_HOST")
-	redisPort := os.Getenv("REDIS_PORT")
-	address := fmt.Sprintf("%s:%s", redisHost, redisPort)
-	comm.InitSessionStore(router, address)
+	comm.InitSessionStore(router)
 
-	rootQuery := g.NewRoot()
-	// Create a new graphql schema, passing in the root query
-	schema, err := graphql.NewSchema(
-		graphql.SchemaConfig{
-			Query: rootQuery.Query,
-			Mutation: rootQuery.Mutation,
-		},
-	)
-
-	if err != nil {
-		fmt.Println("error creating schema: ", err)
-	}
-
-	authController := new(auth.Controller)
-
-	// Set up public level routes
-	router.POST("/login", authController.AuthenticateUser)
-	router.POST("/logout", authController.LogoutUser)
-	router.POST("/public", g.GraphQL(schema))
-	router.OPTIONS("/public", func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Writer.Header().Set("Access-Control-Request-Method","POST, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept-Encoding")
-	})
-
-	// Set up role-protected routes
-	admin := router.Group("/admin")
-	admin.Use(auth.VerifyAuthentication())
+	config.SetupRoutes(router)
 
 	return router, db
 }
