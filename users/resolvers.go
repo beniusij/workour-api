ea1e201237baf3c12abd6bc66f594c458d392cd9
Juozas Joe Beniusis <jb@bluetel.co.uk>
2020-03-15 18:38:04 +0000

    Remove auth resolver

--- users/resolvers.go
+++ users/resolvers.go
@@ -22,13 +22,11 @@ func CreateUserResolver(p g.ResolveParams) (interface{}, error) {
 		return nil, err
 	}
 
-	userId, err := userStruct.Save(userValidator.UserModel)
+	user, err := userStruct.Save(userValidator.UserModel)
 
 	if  err != nil {
 		return nil, err
 	}
-
-	user := User{ID: userId}
 
 	c.Set("status", http.StatusCreated)
 
@@ -46,43 +44,4 @@ func GetUserResolver(p g.ResolveParams) (interface{}, error) {
 	}
 
 	return user, nil
-}
-
-// The resolver evaluates if user with provided email exists, and if so
-// authenticates with provided password. If authentication succeeds a token
-// is created and set in the header, and user non-sensitive details are
-// returned in the response body.
-// TODO remove this resolver and related tests
-func AuthenticateUserResolver(p g.ResolveParams) (interface{}, error) {
-	// Get user by email
-	//email := p.Args["email"].(string)
-	//user, err := GetByEmail(email)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//// Check password
-	//psw := p.Args["password"].(string)
-	//err = user.CheckPassword(psw)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//// Generate token
-	//authToken := authentication.AuthToken{}
-	//token, err := authToken.GenerateToken(email)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//// Set up Session and return it
-	//var session Session
-	//session.Email = user.Email
-	//session.Token = token
-	//
-	//c := p.Context.(*gin.Context)
-	//c.Set("status", http.StatusOK)
-	//
-	//return session, nil
-	return nil, nil
 }
